68019a31f388b003bc7f9c17226202388c9090cd
Rahmadinasitor <102144701+Rahmadinasitor@users.noreply.github.com>
2023-01-18 06:50:01 -1200

    Add files via upload

---
+++ tambah_pengguna.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Biodata</title>
+</head>
+<body bgcolor="teal">
+	<b>FORM TAMBAH DATA</b>
+	<form action="proses_tambah_data.php" method="POST">
+	<table border="1">
+		<tr>
+			<td><center><b>
+				NAMA
+			</center>
+		</b>
+			</td>
+			<td>
+				<INPUT name="namaku" TYPE="TEXT" PLACEHOLDER="nama anda">
+			</td>
+		</tr>
+		<tr>
+			<td><b>
+				PASSWORD
+			</b>
+			</td>
+			<td>
+				<INPUT name="katasandiku" TYPE="PASSWORD" PLACEHOLDER="password anda">
+			</td>
+		</tr>
+		<tr>
+			
+			<td>
+				<INPUT  TYPE="submit" value="Kirim Data">
+			</td>
+		</tr>
+	</table>
+	</form>
+</body>
+</html>
